f7cbf6d6d155eeb2d04a07ea92ea6b27dc609cd6
Roberto Reale <roberto.reale@linux.com>
2017-01-10 10:43:25 +0100

    Bumped version to 0.2.0.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'pyTrivialCache',
-    version = '0.1.0',
+    version = '0.2.0',
     description = "The poor man's API for manipulating a file system cache."
     py_modules = [ 'pyTrivialCache' ],
     author = 'Roberto Reale',
